78ff14806d815889f03d92570c2c01509ab3b782
lumi <shadynyxi@gmail.com>
2026-05-17 16:34:57 +0200

    first part

--- src/main/java/com/lumina/modding/ExampleMod.java
+++ src/main/java/com/lumina/modding/ExampleMod.java
@@ -1,5 +1,6 @@
 package com.lumina.modding;
 
+import com.lumina.modding.item.ModItems;
 import net.neoforged.bus.api.Event;
 import org.slf4j.Logger;
 
@@ -31,31 +32,33 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(ExampleMod.MOD_ID)
 public class ExampleMod {
-    // Define mod id in a common place for everything to reference
+
     public static final String MOD_ID = "luminamodding";
-    // Directly reference a slf4j logger
+
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public ExampleMod(IEventBus modEventBus, ModContainer modContainer) {
-        // Register the commonSetup method for modloading
+
         modEventBus.addListener(this::commonSetup);
-        // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
-        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
+
         NeoForge.EVENT_BUS.register(this);
 
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
+        ModItems.register(modEventBus);
+
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
 
     private void commonSetup(FMLCommonSetupEvent event) {
 
+    }
+
+    private void AddCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            event.accept(ModItems.LUMINENCE);
+        }
     }
 
     @SubscribeEvent
