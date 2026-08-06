05d947371b796d96563d31e336e7dc20b75bb3c7
lumi <shadynyxi@gmail.com>
2026-08-06 22:57:34 +0200

    Datagen done

---
+++ src/main/java/com/lumina/modding/datagen/ModBlockTagProvider.java
@@ -0,0 +1,28 @@
+package com.lumina.modding.datagen;
+
+import com.lumina.modding.ExampleMod;
+import com.lumina.modding.block.ModBlocks;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.neoforged.neoforge.common.crafting.BlockTagIngredient;
+import net.neoforged.neoforge.common.data.BlockTagsProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagProvider extends BlockTagsProvider {
+    public ModBlockTagProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, ExampleMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider provider) {
+        tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(ModBlocks.LUMINENCE_BLOCK.get());
+
+        // tag(BlockTags.NEEDS_IRON_TOOL)
+        //        .add(ModBlocks.BLOCK_NAME.get());
+    }
+}
---
+++ src/main/java/com/lumina/modding/datagen/ModDataMapProvider.java
@@ -0,0 +1,22 @@
+package com.lumina.modding.datagen;
+
+import com.lumina.modding.item.ModItems;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.data.DataMapProvider;
+import net.neoforged.neoforge.registries.datamaps.builtin.FurnaceFuel;
+import net.neoforged.neoforge.registries.datamaps.builtin.NeoForgeDataMaps;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModDataMapProvider extends DataMapProvider {
+    protected ModDataMapProvider(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> lookupProvider) {
+        super(packOutput, lookupProvider);
+    }
+
+    @Override
+    protected void gather(HolderLookup.Provider provider) {
+        // this.builder(NeoForgeDataMaps.FURNACE_FUELS)
+        //        .add(ModItems.ITEM_FUEL.getId(), new FurnaceFuel(1200), false);
+    }
+}
